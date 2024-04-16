67f70a1e8e270e951070b4ced8577f34614b110c
metacogpe <metacog@kakao.com>
2024-04-16 23:27:40 +0900

    Question Entity

---
+++ src/main/java/org/mvp/simpleboard/question/Question.java
@@ -0,0 +1,21 @@
+package org.mvp.simpleboard.question;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+    private LocalDateTime createDate;
+
+
+}
