a0e63cff62352ba25d9d9cc3488847239aa50ee8
metacogpe <metacog@kakao.com>
2024-04-23 14:22:44 +0900

    model

--- src/main/java/org/mvp/simpleboard/SimpleBoardApplication.java
+++ src/main/java/org/mvp/simpleboard/SimpleBoardApplication.java
@@ -1,7 +1,5 @@
 package org.mvp.simpleboard;
 
-import org.mvp.simpleboard.question.QuestionRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/org/mvp/simpleboard/question/QuestionController.java
+++ src/main/java/org/mvp/simpleboard/question/QuestionController.java
@@ -1,15 +1,23 @@
 package org.mvp.simpleboard.question;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 //import org.springframework.web.bind.annotation.ResponseBody;
 
+@RequiredArgsConstructor
 @Controller
 public class QuestionController {
 
+    private final QuestionRepository questionRepository;
+
     @GetMapping("/question/list")
-//    @ResponseBody
-    public String list() {
+    public String list(Model model) {
+        List<Question> questionList = this.questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
         return "question_list";
     }
 }
